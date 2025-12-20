ad3f9d80fb58fe7ada5a24520018d8f0e4e58cd7
DrPerkyLegit <DrPerkyFrLegit@gmail.com>
2025-12-19 20:25:40 -0500

    prevent instant console closing

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
-echo Compiling And Starting SimpleBot
+#!/usr/bin/env sh
+
+echo "Compiling And Starting SimpleBot"
 npx tsx ./src/index.ts
+
+echo ""
+echo "Process exited with code $?"
+read -p "Press Enter to close..."
